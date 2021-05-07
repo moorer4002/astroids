06d84eb8df814402f720c8e50d960dc32ea1989f
moorer4002 <78602724+moorer4002@users.noreply.github.com>
2021-05-07 08:45:52 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 0 + bullet, 50)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 0 + bullet, 50)
+})
+let bullet = 0
+let projectile: Sprite = null
+let ship: Sprite = null
+scene.setBackgroundColor(15)
+ship = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 . . . . . . . . 
+    . . . . . . . 1 . . . . . . . . 
+    . . . . . . 1 . 1 . . . . . . . 
+    . . . . . . 1 . 1 . . . . . . . 
+    . . . . . 1 . . . 1 . . . . . . 
+    . . . . . 1 . . . 1 . . . . . . 
+    . . . . 1 . 1 1 1 . 1 . . . . . 
+    . . . . 1 1 . . . 1 1 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+forever(function () {
+    if (controller.left.isPressed()) {
+        bullet += -3
+    }
+    if (controller.right.isPressed()) {
+        bullet += 3
+    }
+})
